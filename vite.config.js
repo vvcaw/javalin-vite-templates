69a299239b7ca85d3060fb1e28929a3f2725f0bf
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-11-25 00:46:03 +0100

    Added svelte example

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import {defineConfig} from 'vite'
+import {svelte} from "@sveltejs/vite-plugin-svelte";
+
+// We use cleaner to clean the frontend_dist folder before packing it into .jar files.
+// Else the new files will be added and old files will still be included in the build
+import cleaner from 'rollup-plugin-cleaner';
+
+// We need globby to select the javascript entry files in the frontend/pages directory.
+// Unfortunately, rollupjs can not natively handle wildcards here.
+import globby from "globby";
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [svelte()],
+    server: {
+        port: 3000
+    },
+    build: {
+        manifest: true,
+        rollupOptions: {
+            plugins: [
+                cleaner({
+                    targets: [
+                        './frontend_dist/'
+                    ]
+                })
+            ],
+            input: globby.sync("frontend/apps/*.js"),
+            output: {
+                dir: "./frontend_dist"
+            }
+        }
+    }
+})
